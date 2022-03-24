80461866cef57e044cb5a4b1131d98f452aaad2c
Giperx <1512739072@qq.com>
2022-03-24 12:14:29 +0800

    week1-1

---
+++ week1/acwing3346你知道你的ABC吗.cpp
@@ -0,0 +1,16 @@
+//acwing3346ÄãÖªµÀÄãµÄABCÂğ 
+#include<iostream>
+#include<cstring>
+#include<algorithm>
+#include<cstdio>
+using namespace std;
+int a[8];
+
+int  main(){
+    for(int i = 0; i < 7; i++) scanf("%d", &a[i]);
+    
+    sort(a, a + 7);
+    cout<<*a<<' '<<*(a+1)<<' '<<*(a+6) - (*a + *(a+1) )<<endl;
+    
+    return 0;
+}
