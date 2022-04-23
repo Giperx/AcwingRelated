9d4582baa3c7229a95dbb074eda5048a2cf41c1f
Giperx <1512739072@qq.com>
2022-04-23 22:47:32 +0800

    week3-acwing1683困牛放牧 分类讨论

---
+++ week3/acwing1737.cpp
@@ -0,0 +1,22 @@
+//
+// Created by Giperx on 2022/4/23.
+//
+//acwing1683困牛放牧
+// 分类讨论
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <cstdio>
+using namespace std;
+
+int main()
+{
+    int a, b, x, y;
+    cin >> a >> b >> x >> y;
+    int res = abs(a - b);
+    cout << min(res, min(abs(a - x), abs(a - y)) + min(abs(b - x), abs(b - y))) << endl;
+    return 0;
+}
+
+
+
