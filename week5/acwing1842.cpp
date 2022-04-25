d1e07a9b1662d08a8949f997989d4a8454d5b722
Giperx <1512739072@qq.com>
2022-04-25 19:45:50 +0800

    week3-acwing1842牛奶桶 枚举

---
+++ week5/acwing1842.cpp
@@ -0,0 +1,24 @@
+//
+// Created by Giperx on 2022/4/25.
+//
+//acwing1842牛奶桶
+//枚举
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <cstdio>
+using namespace std;
+int x, y, m , res;
+
+int main()
+{
+    cin >> x >> y >> m;
+    for (int i = 0; i <= m / x; ++i)
+        for (int j = 0; j <= (m - i * x) / y; ++j)
+            res = max(res, i * x + j * y);
+
+    cout << res << endl;
+    return 0;
+}
+
+
